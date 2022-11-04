e3ce6bf8533c6bdec77d253034fc01119ee16c38
GnaixEuy <sgghcj@live.com>
2022-11-04 09:37:23 +0800

    :tada: 中软三阶段网盘项目

---
+++ src/main/java/cn/realandy/zrdisk/ZrDiskApplication.java
@@ -0,0 +1,13 @@
+package cn.realandy.zrdisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZrDiskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZrDiskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/realandy/zrdisk/ZrDiskApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.realandy.zrdisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZrDiskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
