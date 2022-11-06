27759388362a1740d529895966f7a4c606bf9931
GnaixEuy <sgghcj@live.com>
2022-11-06 18:56:24 +0800

    :wrench: 代码审计

--- src/main/java/cn/realandy/zrdisk/config/AliPayConfig.java
+++ src/main/java/cn/realandy/zrdisk/config/AliPayConfig.java
@@ -1,22 +1,19 @@
 package cn.realandy.zrdisk.config;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @Data
+@Configuration
+@ConfigurationProperties(prefix = "alipay")
 public class AliPayConfig {
-    @Value("${alipay.appid}")
+
     private String appid;
-    @Value("${alipay.url}")
     private String url;
-    @Value("${alipay.privateKey}")
     private String privateKey;
-    @Value("${alipay.publicKey}")
     private String publicKey;
-    @Value("${alipay.notifyUrl}")
     private String notifyUrl;
-    @Value("${alipay.returnUrl}")
     private String returnUrl;
+
 }
