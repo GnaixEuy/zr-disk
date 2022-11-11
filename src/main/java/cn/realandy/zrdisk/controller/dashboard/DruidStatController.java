e52c50d26f1b6180341c74de25d5f87cc9ab3b1b
GnaixEuy <sgghcj@live.com>
2022-11-11 16:12:01 +0800

    :beer: 开放security 框架重定向限制

--- src/main/java/cn/realandy/zrdisk/controller/dashboard/DruidStatController.java
+++ src/main/java/cn/realandy/zrdisk/controller/dashboard/DruidStatController.java
@@ -4,6 +4,8 @@ import com.alibaba.druid.stat.DruidStatManagerFacade;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
+
 /**
  * <img src="http://blog.gnaixeuy.cn/wp-content/uploads/2022/09/倒闭.png"/>
  *
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DruidStatController {
     @GetMapping("/druid/status")
+    @RolesAllowed(value = {"ROLE_ADMIN"})
     public Object druidStat() {
         // DruidStatManagerFacade#getDataSourceStatDataList 该方法可以获取所有数据源的监控数据，除此之外 DruidStatManagerFacade 还提供了一些其他方法，你可以按需选择使用。
         return DruidStatManagerFacade.getInstance().getDataSourceStatDataList();
